12057dff9248044e4fe126f0084270878e0c7f5b
beratcmn <beratcmn@gmail.com>
2023-05-12 02:44:27 +0300

    Solved 83

---
+++ solutions/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,19 @@
+# Completed
+# Runtime 57 ms Beats 12.90%
+# Memory 16.4 MB Beats 7.54%
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+            
+            if not head:
+                return head
+    
+            current = head
+    
+            while current.next:
+                if current.val == current.next.val:
+                    current.next = current.next.next
+                else:
+                    current = current.next
+    
+            return head
