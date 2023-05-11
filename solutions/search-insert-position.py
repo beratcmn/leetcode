ff6f1fb5560756fcf64c755c042c9cd65d92706a
beratcmn <beratcmn@gmail.com>
2023-05-12 02:28:55 +0300

    Typo

--- solutions/search-insert-position.py
+++ solutions/search-insert-position.py
@@ -1,8 +1,5 @@
-from ast import List
-
-
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: list[int], target: int) -> int:
         
         if target in nums:
             return nums.index(target)
