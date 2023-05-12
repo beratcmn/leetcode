9926ea215d0f374ebe816b866ae39a7f67f4235c
beratcmn <beratcmn@gmail.com>
2023-05-12 03:23:37 +0300

    Solved 13

---
+++ solutions/roman_to_integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        pairs = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L": 50,
+            "X": 10,
+            "V": 5,
+            "I": 1,
+        }
+
+        ans = 0
+        for i in range(len(s)-1, -1, -1):
+            num = pairs[s[i]]
+            if 4 * num < ans:
+                ans -= num
+            else:
+                ans += num
+        return ans
