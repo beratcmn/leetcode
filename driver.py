e9536a7c6f9bc4564f920f62bedb5560365ac7f5
beratcmn <beratcmn@gmail.com>
2023-05-12 03:14:49 +0300

    Solved

--- driver.py
+++ driver.py
@@ -1,1 +1,12 @@
-from solutions import *
+from solutions.merge_sorted_array import Solution as merge_sorted_array
+
+solution = merge_sorted_array()
+
+result = solution.merge(
+    nums1=[1,2,3,0,0,0], 
+    m=3,
+    nums2=[2,5,6],
+    n=3
+    )
+
+print(result)
--- solutions/merge_sorted_array.py
+++ solutions/merge_sorted_array.py
@@ -1,4 +1,6 @@
-from ast import List
+# Completed
+# Runtime 51 ms Beats 20.67%
+# Memory 16.4 MB Beats 10.49%
 
 
 class Solution:
@@ -7,14 +9,15 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        
+
         if m == 0 and n != 0:
             for i in range(len(nums1)):
                 nums1[i] = nums2[i]
         elif m != 0 and n == 0:
             nums1 = nums1
         elif m != 0 and n != 0:
-            joined_ul = list(nums1[0:m] + nums2).sort()
+            joined_ul = nums1[0:m] + nums2
+            joined_ul.sort()
         
             for i in range(len(nums1)):
                 nums1[i] = joined_ul[i]
