dd2f885b3556c009cded922b590c0fc7c656f1a9
beratcmn <beratcmn@gmail.com>
2023-05-16 02:02:20 +0300

    Solved 100

---
+++ solutions/same_tree.py
@@ -0,0 +1,9 @@
+# Runtime 52 ms Beats 7.20%
+# Memory 16.3 MB Beats 12.35%
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        elif p is not None and q is not None:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
