ec8b86eb405357e8d2a0c081819d0739f6172ed0
beratcmn <beratcmn@gmail.com>
2023-05-23 02:13:18 +0300

    Solved 12

---
+++ solutions/integer_to_roman.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        roman = ""
+
+        for i in range(len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                roman += symbols[i]
+
+        return roman
