79b8cf6d064c77095810473e09bab5e29757ea48
beratcmn <beratcmn@gmail.com>
2023-05-28 02:19:50 +0300

    Solved 1

--- solutions/csharp/Program.cs
+++ solutions/csharp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TwoSum;
+
+Solution s = new Solution();
+
+int[] nums = new int[] { 2, 7, 11, 15 };
+int target = 9;
+
+int[] result = s.TwoSum(nums, target);
+
+Console.WriteLine($"[{result[0]}, {result[1]}]");
