441f40681ca2b23478d75cb0539979b849165c85
beratcmn <beratcmn@gmail.com>
2023-05-28 23:13:35 +0300

    Fixed formatting

--- solutions/csharp/LetterCombinationsOfAPhoneNumber.cs
+++ solutions/csharp/LetterCombinationsOfAPhoneNumber.cs
@@ -1,12 +1,9 @@
 // Runtime 148 ms Beats 41.17%
 // Memory 44 MB Beats 36.93%
 
-namespace LetterCombinationsOfAPhoneNumber
-{
-    public class Solution
-    {
-        public IList<string> LetterCombinations(string digits)
-        {
+namespace LetterCombinationsOfAPhoneNumber {
+    public class Solution {
+        public IList<string> LetterCombinations(string digits) {
             if (digits == "")
                 return new List<string>();
 
@@ -24,16 +21,11 @@ namespace LetterCombinationsOfAPhoneNumber
 
             List<string> pairs = new List<string>();
 
-            void Backtrack(string combination, string nextDigits)
-            {
-                if (nextDigits.Length == 0)
-                {
+            void Backtrack(string combination, string nextDigits) {
+                if (nextDigits.Length == 0) {
                     pairs.Add(combination);
-                }
-                else
-                {
-                    foreach (char letter in keyboard[nextDigits[0]])
-                    {
+                } else {
+                    foreach (char letter in keyboard[nextDigits[0]]) {
                         Backtrack(combination + letter, nextDigits.Substring(1));
                     }
                 }
--- solutions/csharp/TwoSum.cs
+++ solutions/csharp/TwoSum.cs
@@ -1,22 +1,16 @@
 // Runtime 370 ms Beats 5.6%
 // Memory 60.5 MB Beats 5.25%
 
-namespace TwoSum
-{
-    public class Solution
-    {
-        public int[] TwoSum(int[] nums, int target)
-        {
-            for (int i = 0; i < nums.Length; i++)
-            {
+namespace TwoSum {
+    public class Solution {
+        public int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++) {
                 int[] newList = new int[nums.Length];
                 Array.Copy(nums, newList, nums.Length);
                 newList[Array.IndexOf(nums, nums[i])] = 9999;
 
-                for (int j = 0; j < newList.Length; j++)
-                {
-                    if (nums[i] + newList[j] == target)
-                    {
+                for (int j = 0; j < newList.Length; j++) {
+                    if (nums[i] + newList[j] == target) {
                         return new int[] { i, Array.IndexOf(newList, newList[j]) };
                     }
                 }
